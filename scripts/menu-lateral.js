c1c0d2aa669560017d5ca9ba2c9a39325535902c
Joel <master.deus23@gmail.com>
2024-12-15 17:11:33 -0600

    Responsive seems to be right

--- scripts/menu-lateral.js
+++ scripts/menu-lateral.js
@@ -1,9 +1,9 @@
 // Seleccionar el ícono de las barras (hamburguesa) y el menú lateral
-//const menuIcon = document.querySelector('.topnavbar .fa-bars');
-//const menuLateral = document.querySelector('.menu-lateral');
+const menuIcon = document.querySelector('.topnavbar .fa-bars');
+const menuLateral = document.querySelector('.menu-lateral');
 
-// Añadir un evento de clic al ícono de la hamburguesa
-//menuIcon.addEventListener('click', () => {
+  // Añadir un evento de clic al ícono de la hamburguesa
+  menuIcon.addEventListener('click', () => {
   // Alternar la clase "active" en el menú lateral
-  //menuLateral.classList.toggle('active');
+  menuLateral.classList.toggle('active');
 });
