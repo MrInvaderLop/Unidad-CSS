cf88a36a1db7744b4b06e89597185d67caf026a1
Joel <master.deus23@gmail.com>
2024-12-15 17:19:08 -0600

    Page seems to be ready

--- scripts/menu-lateral.js
+++ scripts/menu-lateral.js
@@ -1,6 +1,7 @@
 // Seleccionar el ícono de las barras (hamburguesa) y el menú lateral
 const menuIcon = document.querySelector('.topnavbar .fa-bars');
 const menuLateral = document.querySelector('.menu-lateral');
+const mainActive = document.querySelector('.main-active');
 
   // Añadir un evento de clic al ícono de la hamburguesa
   menuIcon.addEventListener('click', () => {
